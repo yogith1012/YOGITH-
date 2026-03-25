a20b80b5995818c5f9decd5a28bf42035b8cd2ae
Yogith <yogith3553@gmail.com>
2026-03-25 18:10:22 +0530

    Create even_position_sum.c for summing even positions
    Added a new C program to sum elements at even positions.

---
+++ even_position_sum.c
@@ -0,0 +1,13 @@
+# C Programming Repository
+
+This repository contains basic C programs for learning and practice.
+
+## Programs Included
+1. Sum of even position elements
+2. (More programs coming...)
+
+## Language
+C
+
+## Author
+Yogith
